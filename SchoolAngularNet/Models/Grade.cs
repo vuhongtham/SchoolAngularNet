09d6eb9d72b2270aad42e95f25f3a22ec638badd
vht11 <vht11@DESKTOP-ODQKIUH>
2020-02-01 12:55:16 +0700

    breakwork

--- SchoolAngularNet/Models/Grade.cs
+++ SchoolAngularNet/Models/Grade.cs
@@ -8,7 +8,8 @@ namespace SchoolAngularNet.Models
     public class Grade
     {
         public int GradeId { get; set; }
-        public string GradeName { get; set; }
+        public string GradeName { get; set; } 
+
 
         public ICollection<Student> Students { get; set; }
     }
